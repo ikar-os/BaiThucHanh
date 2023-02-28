2d0c53beffcb5fbc626ec597549469827065d424
ikar-os <c0376793075@gmail.com>
2023-02-28 11:06:47 +0700

    slide55

---
+++ BaiThucHanh2802/src/Slide55.java
@@ -0,0 +1,12 @@
+
+public class Slide55 {
+    public static void main(String[] args){
+        int i = 0, number = 20;
+        for(; i <= number ; i++)
+            if(i % 5 == 0){
+                System.out.println(i);
+                continue;
+            }
+    System.out.print("cac so tren la cac so chia het cho 5 trong pham vi tu 0 den 20");
+    }
+}
