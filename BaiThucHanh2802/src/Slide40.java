b5f344c67a7e2fa44facdf2b611c95ae02cb2f37
ikar-os <c0376793075@gmail.com>
2023-02-28 11:40:31 +0700

    Slide40 update

--- BaiThucHanh2802/src/Slide40.java
+++ BaiThucHanh2802/src/Slide40.java
@@ -5,35 +5,33 @@ public class Slide40 {
         int ngay;
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Nhap so ngay trong tuan (so ngay tu 0-7): ");
+        System.out.print("Nhap so ngay trong tuan (so ngay tu 1-7): ");
         ngay = sc.nextInt();
         switch(ngay){
-            case 0:
-            System.out.println("Hom nay la ngay chu nhat");
-            break;
             case 1:
-            System.out.println("Hom nay la ngay thu hai");
+            System.out.println("Hom nay la ngay chu nhat");
             break;
             case 2:
-            System.out.println("Hom nay la ngay thu ba");
+            System.out.println("Hom nay la ngay thu hai");
             break;
             case 3:
-            System.out.println("Hom nay la ngay thu tu");
+            System.out.println("Hom nay la ngay thu ba");
             break;
             case 4:
-            System.out.println("Hom nay la ngay thu nam");
+            System.out.println("Hom nay la ngay thu tu");
             break;
             case 5:
-            System.out.println("Hom nay la ngay thu sau");
+            System.out.println("Hom nay la ngay thu nam");
             break;
             case 6:
-            System.out.println("Hom nay la ngay thu bay");
+            System.out.println("Hom nay la ngay thu sau");
             break;
             case 7:
-            System.out.println("Hom nay la ngay chu nhat");
+            System.out.println("Hom nay la ngay thu bay");
             break;
             default:
             System.out.println("So ngay khong hop le!");
         }
+        sc.close();
     }
 }
