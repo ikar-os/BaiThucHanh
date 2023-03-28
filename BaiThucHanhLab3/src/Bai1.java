b70a567664a80e328e287ea26c7a16c8ac2b6edf
ikar-os <c0376793075@gmail.com>
2023-03-28 09:26:05 +0700

    bai thuc hanh 21/03 (sua loi)

---
+++ BaiThucHanhLab3/src/Bai1.java
@@ -0,0 +1,9 @@
+public class Bai1 {
+    public static void main(String[] args) throws Exception {
+        hinhTron ht = new hinhTron();
+            ht.nhapBanKinh();
+            ht.tinhChuvi();
+            ht.tinhDienTich();
+            ht.inThongso();
+    }
+}
---
+++ BaiThucHanhLab3/src/Bai2.java
@@ -0,0 +1,8 @@
+public class Bai2 {
+    public static void main(String[] args) {
+        sinhVien sv = new sinhVien();
+        sv.nhapThongTinSv();
+        sv.tinhTuoi();
+        sv.inThongTinSv();
+    }
+}
